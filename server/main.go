e67e254ad2acb12b1d425c9375da80589b964fb8
shitij <21.shitijagrawal@gmail.com>
2021-04-08 00:39:41 +0530

    reformat

--- server/main.go
+++ server/main.go
@@ -13,6 +13,53 @@ import (
 	"sync"
 )
 
+func SyncWithBasicCategories(dataBase structures.DataBase, group *sync.WaitGroup) {
+	defaultDataCollection := mongodb.OpenDefaultDataCollection(dataBase.Mongo)
+
+	defer group.Done()
+
+	defaultStream, err := defaultDataCollection.Watch(context.TODO(), mongo.Pipeline{}, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+	if err != nil {
+		panic(err)
+	}
+	defer defaultStream.Close(context.TODO())
+
+	for defaultStream.Next(context.TODO()) {
+		var data bson.M
+		if err = defaultStream.Decode(&data); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%v\n", data)
+
+		operationType := data["operationType"].(string)
+
+		if operationType == "insert" || operationType == "update" || operationType == "replace" {
+			fullDocument := data["fullDocument"].(bson.M)
+			category := fullDocument["_id"].(string)
+			subCategory := fullDocument["sub_categories"].(bson.A)
+
+			array := make([]string, len(subCategory))
+			for i, ele := range subCategory {
+				array[i] = ele.(string)
+			}
+
+			categoriesData := structures.BasicCategoriesData{
+				Category:      category,
+				SubCategories: array,
+			}
+
+			dataBase.Redis.HSet(context.TODO(), "categories", category, categoriesData.Marshal())
+
+		} else if operationType == "delete" {
+			documentKey := data["documentKey"].(bson.M)
+			id := documentKey["_id"].(string)
+
+			dataBase.Redis.HDel(context.TODO(), "categories", id)
+
+		}
+	}
+}
+
 func main() {
 
 	dataBase := structures.DataBase{
@@ -24,52 +71,7 @@ func main() {
 
 	waitGroup.Add(1)
 
-	go func(dataBase structures.DataBase, group *sync.WaitGroup) {
-		defaultDataCollection := mongodb.OpenDefaultDataCollection(dataBase.Mongo)
-
-		defer waitGroup.Done()
-
-		defaultStream, err := defaultDataCollection.Watch(context.TODO(), mongo.Pipeline{}, options.ChangeStream().SetFullDocument(options.UpdateLookup))
-		if err != nil {
-			panic(err)
-		}
-		defer defaultStream.Close(context.TODO())
-
-		for defaultStream.Next(context.TODO()) {
-			var data bson.M
-			if err = defaultStream.Decode(&data); err != nil {
-				panic(err)
-			}
-			fmt.Printf("%v\n", data)
-
-			operationType := data["operationType"].(string)
-
-			if operationType == "insert" || operationType == "update" || operationType == "replace" {
-				fullDocument := data["fullDocument"].(bson.M)
-				category := fullDocument["_id"].(string)
-				subCategory := fullDocument["sub_categories"].(bson.A)
-
-				array := make([]string, len(subCategory))
-				for i, ele := range subCategory {
-					array[i] = ele.(string)
-				}
-
-				categoriesData := structures.BasicCategoriesData{
-					Category:      category,
-					SubCategories: array,
-				}
-
-				dataBase.Redis.HSet(context.TODO(), "categories", category, categoriesData.Marshal())
-
-			} else if operationType == "delete" {
-				documentKey := data["documentKey"].(bson.M)
-				id := documentKey["_id"].(string)
-
-				dataBase.Redis.HDel(context.TODO(), "categories", id)
-
-			}
-		}
-	}(dataBase, &waitGroup)
+	go SyncWithBasicCategories(dataBase, &waitGroup)
 
 	waitGroup.Wait()
 
